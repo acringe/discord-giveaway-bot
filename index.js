89ff833b268c217bbaa8aebcab2997a339e1ee8f
Joe Lee Official <70639311+joeleeofficial@users.noreply.github.com>
2020-12-22 19:23:55 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+let fs = require('fs');
+const PREFIX = "YOUR_DISCORD_BOT_PREFIX";
+const TOKEN = "YOUR_DISCORD_BOT_TOKEN";
+
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.name, command);
+}
+
+client.on('message', async message => {
+  if (message.content.startsWith(`PREFIX`)) {
+    let file_name = `${message.content.split(' ')[0].replace(prefix, '')}.js`;
+    if(!fs.existsSync('./commands/' + file_name)) return undefined;
+    if(fs.existsSync('./commands/' + file_name)) {
+      client.commands.get(file_name.replace('.js', '')).execute(client, message);
+    }
+  }
+});
+
+client.login(TOKEN);
